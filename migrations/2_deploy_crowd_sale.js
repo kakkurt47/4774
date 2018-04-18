cb3a1aeef4312af98cb2cbcf4d470badc06d404d
Jeong Chaehong <brcps12@gmail.com>
2018-04-18 20:41:25 +0900

    fix(deploy): fix deploy script

--- migrations/2_deploy_crowd_sale.js
+++ migrations/2_deploy_crowd_sale.js
@@ -1,5 +1,8 @@
-const MuzikaCoin = artifacts.require("MuzikaCoin")
+const MuzikaCoin = artifacts.require("MuzikaCoin");
+const MuzikaPaperContract = artifacts.require('MuzikaPaperContract');
 
 module.exports = (deployer, network, accounts) => {
-  deployer.deploy(MuzikaCoin);
+  deployer.deploy(MuzikaCoin, 1e+27).then(() => {
+      return deployer.deploy(MuzikaPaperContract, MuzikaCoin.address, Date.now());
+  });
 };
--- migrations/3_deploy_muzika_paper_contract.js
+++
@@ -1,8 +0,0 @@
-const MuzikaPaperContract = artifacts.require('MuzikaPaperContract');
-const MuzikaCoin = artifacts.require('MuzikaCoin');
-
-module.exports = (deployer) => {
-  MuzikaCoin.deployed().then(ins => {
-      return deployer.deploy(MuzikaPaperContract, ins.address, Date.now());
-  });
-};
