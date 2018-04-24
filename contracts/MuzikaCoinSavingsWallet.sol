e511ea5ac478190a04e3b33fc36cf075d47bc681
Jeong Chaehong <brcps12@gmail.com>
2018-04-24 20:06:30 +0900

    rearrange code

--- contracts/MuzikaCoinSavingsWallet.sol
+++ contracts/MuzikaCoinSavingsWallet.sol
@@ -4,26 +4,26 @@ import "../zeppelin-solidity/contracts/ownership/Heritable.sol";
 
 contract MuzikaCoinSavingsWallet is Heritable {
 
-    event Sent(address indexed payee, uint256 amount, uint256 balance);
-    event Received(address indexed payer, uint256 amount, uint256 balance);
+  event Sent(address indexed payee, uint256 amount, uint256 balance);
+  event Received(address indexed payer, uint256 amount, uint256 balance);
 
 
-    constructor(uint256 _heartbeatTimeout) Heritable(_heartbeatTimeout) public {}
+  constructor(uint256 _heartbeatTimeout) Heritable(_heartbeatTimeout) public {}
 
-    /**
-     * @dev wallet can receive funds.
-     */
-    function () public payable {
-        emit Received(msg.sender, msg.value, address(this).balance);
-    }
+  /**
+   * @dev wallet can receive funds.
+   */
+  function() public payable {
+    emit Received(msg.sender, msg.value, address(this).balance);
+  }
 
-    /**
-     * @dev wallet can send funds
-     */
-    function sendTo(address payee, uint256 amount) public onlyOwner {
-        require(payee != 0 && payee != address(this));
-        require(amount > 0);
-        payee.transfer(amount);
-        emit Sent(payee, amount, address(this).balance);
-    }
+  /**
+   * @dev wallet can send funds
+   */
+  function sendTo(address payee, uint256 amount) public onlyOwner {
+    require(payee != 0 && payee != address(this));
+    require(amount > 0);
+    payee.transfer(amount);
+    emit Sent(payee, amount, address(this).balance);
+  }
 }
--- contracts/token/MuzikaCoin.sol
+++ contracts/token/MuzikaCoin.sol
@@ -5,20 +5,20 @@ import '../../zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol';
 
 // @TODO Support Freeze account
 contract MuzikaCoin is MintableToken, BurnableToken {
-    string public name = "MUZIKA COIN";
-    string public symbol = "MZK";
-    uint8 public decimals = 18;
+  string public name = "MUZIKA COIN";
+  string public symbol = "MZK";
+  uint8 public decimals = 18;
 
-    /**
-     * @dev Constructor that gives msg.sender all of existing tokens.
-     */
-    constructor(uint256 initialSupply) public {
-        totalSupply_ = initialSupply;
-        balances[msg.sender] = initialSupply;
-        emit Transfer(0x0, msg.sender, initialSupply);
-    }
+  /**
+   * @dev Constructor that gives msg.sender all of existing tokens.
+   */
+  constructor(uint256 initialSupply) public {
+    totalSupply_ = initialSupply;
+    balances[msg.sender] = initialSupply;
+    emit Transfer(0x0, msg.sender, initialSupply);
+  }
 
-    function burn(uint256 _amount) public onlyOwner {
-        super.burn(_amount);
-    }
+  function burn(uint256 _amount) public onlyOwner {
+    super.burn(_amount);
+  }
 }
