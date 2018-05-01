44369dcea208b4d64650434db870e74ea056d3a0
Heo Sangmin <heo@mapiacompany.com>
2018-05-01 21:11:31 +0900

    ci(gitlab): setup registry image for blockchain

--- projects/marketplace/src/app/page/test/test.component.ts
+++ projects/marketplace/src/app/page/test/test.component.ts
@@ -57,7 +57,7 @@ export class TestPageComponent extends BaseComponent {
               });
               observer.complete();
             });
-          })
+          });
         });
       })
     ).subscribe(
