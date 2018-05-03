98964bd9bf77ed1d28fefcd8673b1068e3d324ef
Heo Sangmin <heo@mapiacompany.com>
2018-05-03 11:32:01 +0900

    refactor(migrations): move deploy_paper_contract.js to contract-repo

--- migrations/2_deploy_muzika_coin.js
+++
@@ -1,8 +0,0 @@
-const MuzikaCoin = artifacts.require("MuzikaCoin");
-
-module.exports = (deployer) => {
-	const decimals = 18;
-	const initialSupply = 1e8;
-
-	deployer.deploy(MuzikaCoin, initialSupply * (10 ** decimals));
-};
