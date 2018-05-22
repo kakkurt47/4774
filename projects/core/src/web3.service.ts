d02762d47243538070e10ca5ccb3edb2f91ee3d6
Brcps <brcps12@gmail.com>
2018-05-23 00:20:09 +0900

    feat(core): implement getTransaction()

--- projects/core/src/web3.service.ts
+++ projects/core/src/web3.service.ts
@@ -4,10 +4,12 @@ import {EnvironmentToken, EnvironmentType} from './environments/env_types';
 import {promisify} from './utils';
 import {AddressOnlyProvider, LedgerProvider, MetamaskProvider, RPCProvider, WalletProvider} from './web3-providers';
 import {ExtendedWeb3} from './web3.provider';
+import {MuzikaCoin} from './contracts';
 
 @Injectable()
 export class MuzikaWeb3Service {
   constructor(private web3: ExtendedWeb3,
+              private muzikaCoin: MuzikaCoin,
               @Inject(EnvironmentToken) private environment: EnvironmentType,
               @Inject('RPC_URL') private rpcUrl: string) {
   }
@@ -57,14 +59,37 @@ export class MuzikaWeb3Service {
     this.web3.setProvider(new AddressOnlyProvider(address, this.rpcUrl));
   }
 
-  public getTransactions(): Observable<any[]> {
+  public getTransactions(address: string): Observable<any[]> {
     const subj = new BehaviorSubject<any[]>([]);
+    const transferHash = this.web3.sha3('Transfer(address,address,uint256)');
 
-    /** @TODO
-     * @assign brcps12
-     * @implements use etherscan.io public api or use our internal ethereum node
-     * Get Transaction list & return all of the transactions as array
-     */
+    // Padding zeros to left (length of data set to 32 bytes)
+    address = '0x' + (<any>this.web3).padLeft(address.replace('0x', ''), 64);
+    const filter = this.web3.eth.filter({
+      // @TODO fromBlock must be the number of block at which MuzikaCoin is created
+      fromBlock: 0,
+      toBlock: 'latest',
+      address: this.muzikaCoin.address,
+      // Transfer `address` to another
+      topics: [transferHash, address]
+      // Receive Tokens Events
+      // topics: [transferHash, null, address]
+    });
+
+    (<any>filter).get((error: any, events: any) => {
+      if (error) {
+
+      }
+
+      // the order is ascend of block number
+      subj.next(events.map(event => {
+        return {
+          from: event.topics[1],
+          to: event.topics[2],
+          amount: this.web3.toBigNumber(event.data).toString()
+        };
+      }));
+    });
 
     return subj.asObservable();
   }
