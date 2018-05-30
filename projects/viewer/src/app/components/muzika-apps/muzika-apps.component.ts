0028c64cd4460280dc2376a6904e935cfb8b9734
Heo Sangmin <heo@mapiacompany.com>
2018-05-31 00:44:08 +0800

    refactor(muzika-apps): detect NavigationEnd & floating-wallet case in router

--- projects/viewer/src/app/components/muzika-apps/muzika-apps.component.ts
+++ projects/viewer/src/app/components/muzika-apps/muzika-apps.component.ts
@@ -1,20 +1,37 @@
 import {Component, EventEmitter, Output} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute, Router, NavigationEnd} from '@angular/router';
+import {BaseComponent} from '@muzika/core';
 
 @Component({
   selector: 'muzika-apps',
   templateUrl: './muzika-apps.component.html',
   styleUrls: ['./muzika-apps.component.scss']
 })
-export class MuzikaAppsComponent {
-  @Output() tabChange: EventEmitter<'viewer' | 'wallet'> = new EventEmitter();
-  currentTab: 'viewer' | 'wallet' = 'viewer';
+export class MuzikaAppsComponent extends BaseComponent {
+  @Output() tabChange: EventEmitter<'viewer' | 'wallet' | 'floating-wallet'> = new EventEmitter();
+  currentTab: 'viewer' | 'wallet' | 'floating-wallet' = 'viewer';
 
   constructor(private router: Router,
               private route: ActivatedRoute) {
+    super();
   }
 
-  changeTab(tab: 'viewer' | 'wallet') {
+  ngOnInit() {
+    super.ngOnInit();
+    this._sub.push(
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          // @TODO @brcps12 You should change this router url to signing url
+          // (or change it to regex patterns of urls which should be shown as floating modal)
+          if (event.url === '/(wallet:home)') {
+            this.changeTab('floating-wallet');
+          }
+        }
+      })
+    );
+  }
+
+  changeTab(tab: 'viewer' | 'wallet' | 'floating-wallet') {
     this.currentTab = tab;
     this.tabChange.emit(tab);
   }
