161a736988bd9fb5b5b21048dbf5e96a8b73a581
Jeong Chaehong <brcps12@gmail.com>
2018-05-31 16:12:46 +0900

    refactor(login): move login page in wallet module to main module

--- projects/viewer/src/app/app-routing.module.ts
+++ projects/viewer/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginPageComponent} from './pages/login/login.component';
 import {MainPageComponent} from './pages/main/main.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
+  { path: 'login', component: LoginPageComponent },
 ];
 
 @NgModule({
