887f6896c945db62c5d3fac3cfa54b111933ab55
Jeong Chaehong <brcps12@gmail.com>
2018-06-01 15:41:00 +0900

    feat(viewer): view balances

--- projects/viewer/src/app/pages/main/main.component.ts
+++ projects/viewer/src/app/pages/main/main.component.ts
@@ -1,5 +1,7 @@
+import {select} from '@angular-redux/store';
 import {Component} from '@angular/core';
-import {BaseComponent, CommunityPost, SheetPost} from '@muzika/core';
+import {BaseComponent, CommunityPost, ExtendedWeb3, MuzikaCoin, SheetPost, unitUp, User} from '@muzika/core';
+import {Observable, combineLatest, from} from 'rxjs';
 import {BestPostsMock, SheetPostsMock} from '../../../mock/posts';
 
 @Component({
@@ -13,8 +15,39 @@ export class MainPageComponent extends BaseComponent {
 
   topSheets: SheetPost[];
 
-  constructor() {
+  @select(['user', 'currentUser'])
+  currentUserObs: Observable<User>;
+  currentUser: User;
+  balances: {
+    mzk: number | string;
+    dollar: number | string;
+  };
+
+  constructor(private muzikaCoin: MuzikaCoin,
+              private web3: ExtendedWeb3) {
     super();
     this.topSheets = this.sheets.slice(0, 3);
+    this.balances = <any>{};
+  }
+
+  ngOnInit() {
+    this.muzikaCoin.deployed();
+
+    this._sub.push(
+      combineLatest(
+        from(this.muzikaCoin.deployed()),
+        this.currentUserObs
+      ).subscribe(async ([coin, user]) => {
+        console.log('33', user);
+        if (user) {
+          const rawBalance = await coin.balanceOf(user.address);
+          this.balances = {
+            mzk: unitUp(rawBalance),
+            // @TODO deploy muzika loyalty point
+            dollar: 0
+          };
+        }
+      })
+    );
   }
 }
