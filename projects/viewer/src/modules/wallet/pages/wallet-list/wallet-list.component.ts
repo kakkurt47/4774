441efc2cb5d59643456feeff91932fd8b36e7d85
Jeong Chaehong <brcps12@gmail.com>
2018-06-01 15:39:59 +0900

    feat(wallet): add token transfer and view balances

--- projects/viewer/src/modules/wallet/pages/wallet-list/wallet-list.component.ts
+++ projects/viewer/src/modules/wallet/pages/wallet-list/wallet-list.component.ts
@@ -1,21 +1,54 @@
-import {Component, OnInit} from '@angular/core';
-import {BaseComponent} from '@muzika/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {BaseComponent, promisify, toBigNumber, unitDown, unitUp} from '@muzika/core';
 import * as ethUtil from 'ethereumjs-util';
 import * as ethWallet from 'ethereumjs-wallet';
+import {createTruffleMuzikaCoin, IMuzikaCoin} from '../../../../../../core/src/contracts/interface/MuzikaCoin';
+import {AlertService} from '../../../alert/alert.service';
 import {WalletStorageService} from '../../services/wallet-storage.service';
+import {Web3WalletProvider} from '../../wallet.provider';
 
 @Component({
   selector: 'wallet-list-page',
   templateUrl: './wallet-list.component.html',
-  styleUrls: ['./wallet-list.component.scss']
+  styleUrls: ['./wallet-list.component.scss'],
+  providers: [Web3WalletProvider]
 })
 export class WalletListComponent extends BaseComponent {
-  accounts: string[] = [];
+  accounts: {
+    address: string,
+    eth: string,
+    mzk: string
+  }[] = [];
   selectedAccount: string;
-  tx: any = {};
+  tx: {
+    to: string;
+    value: string | number;
+    gas: string | number;
+    gasPrice: string | number;
+  };
+  web3: any;
 
-  constructor(private walletStorage: WalletStorageService) {
+  sentTxHash: string;
+  private coin: IMuzikaCoin;
+  private _submitted = false;
+
+  constructor(private walletStorage: WalletStorageService,
+              private walletProvider: Web3WalletProvider,
+              private alertService: AlertService,
+              private zone: NgZone) {
     super();
+    walletProvider.create(block => {
+      zone.run(() => {
+        this.loadBalances();
+      });
+    });
+
+    const MuzikaCoin = createTruffleMuzikaCoin();
+    MuzikaCoin.setProvider(walletProvider);
+    MuzikaCoin.deployed().then(deployed => {
+      this.coin = deployed;
+      this.loadBalances();
+    });
   }
 
   ngOnInit() {
@@ -24,10 +57,28 @@ export class WalletListComponent extends BaseComponent {
         this.accounts = privateKeys.map(key => {
           const address = ethUtil.privateToAddress(ethUtil.toBuffer(key));
 
-          return ethUtil.toChecksumAddress(ethUtil.bufferToHex(address));
+          return {
+            address: ethUtil.toChecksumAddress(ethUtil.bufferToHex(address)),
+            eth: null,
+            mzk: null
+          };
         });
+
+        this.loadBalances();
       })
     );
+
+    this.tx = {
+      to: '',
+      gas: '',
+      value: '',
+      gasPrice: ''
+    };
+  }
+
+  ngOnDestroy() {
+    super.ngOnDestroy();
+    this.walletProvider.stop();
   }
 
   createWallet(): void {
@@ -35,7 +86,85 @@ export class WalletListComponent extends BaseComponent {
     this.walletStorage.addWallet(ethUtil.bufferToHex(wallet.getPrivateKey()));
   }
 
-  sendMZK(): void {
-    // @TODO Implement send token and get balance of accounts
+  sendMZK() {
+    const tx = this.tx;
+    if (this._submitted) {
+      this.alertService.alert('Already submitted. Please wait for a second');
+      return;
+    }
+
+    if (!tx.to || !tx.value) {
+      this.alertService.alert('Please fill `To` and `Amount`');
+      return;
+    }
+
+    this.alertService.confirm(
+      "Are you sure to transfer? Please make sure that you provided information is correct.",
+      async () => {
+        this._submitted = true;
+        try {
+          const gasPrice = unitDown(tx.gasPrice, 9); // gwei: 9
+          const mzk = unitDown(tx.value);
+          this.sentTxHash = await this.coin.transfer.sendTransaction(tx.to, mzk, {
+            from: this.selectedAccount,
+            gas: tx.gas,
+            gasPrice: gasPrice
+          });
+          this.tx = {
+            to: '',
+            gas: '',
+            value: '',
+            gasPrice: ''
+          };
+          this._submitted = false;
+        } catch (e) {
+          this.alertService.alert('Out of gas or your Muzika is not enough to transfer');
+          console.error(e);
+        } finally {
+          this._submitted = false;
+        }
+      }
+    );
+  }
+
+  selectAccount(address: string) {
+    this.walletProvider.changeAddress(address);
+    this.selectedAccount = address;
+  }
+
+  async loadBalances() {
+    if (this.coin) {
+      this.accounts.forEach(async (account, index) => {
+        this.accounts[index].eth = unitUp(await this.ethBalanceOf(account.address));
+        this.accounts[index].mzk = unitUp(await this.coin.balanceOf(account.address));
+      });
+    }
+  }
+
+  estimate() {
+    if (this.tx.to && this.tx.value) {
+      this._estimateGas(this.tx, true);
+    }
+  }
+
+  ethBalanceOf(address: string): Promise<string> {
+    const params = {
+      jsonrpc: '2.0',
+      method: 'eth_getBalance',
+      params: [address, 'latest']
+    };
+
+    return promisify(this.walletProvider.sendAsync.bind(this.walletProvider), params).then(v => {
+      return toBigNumber(v.result).toString(10);
+    });
+  }
+
+  private _estimateGas(tx: any, set?: boolean): Promise<number> {
+    return this.coin.transfer.estimateGas(tx.to, unitDown(tx.value)).then(estimated => {
+      if (set) {
+        this.tx.gas = estimated;
+      }
+      return estimated;
+    });
   }
 }
---
+++ projects/viewer/src/modules/wallet/wallet.provider.ts
@@ -0,0 +1,57 @@
+import {Inject, Injectable} from '@angular/core';
+import {BlockWithTransactionData, Provider as Web3Provider} from '@0xproject/types';
+import * as ProviderEngine from 'web3-provider-engine';
+import * as CacheSubprovider from'web3-provider-engine/subproviders/cache';
+import * as FilterSubprovider from'web3-provider-engine/subproviders/filters';
+import * as HookedWalletEthTxSubprovider from'web3-provider-engine/subproviders/hooked-wallet-ethtx';
+import * as RpcSubprovider from 'web3-provider-engine/subproviders/rpc';
+import {IMuzikaCoin} from '../../../../core/src/contracts/interface/MuzikaCoin';
+import {WalletStorageService} from './services/wallet-storage.service';
+
+@Injectable()
+export class Web3WalletProvider implements Web3Provider {
+  private engine: ProviderEngine;
+  private selectedAddress: string;
+
+  constructor(@Inject('RPC_URL') private rpcUrl,
+              private walletStorage: WalletStorageService) {
+  }
+
+  create(blockTracker?: (block: BlockWithTransactionData) => any) {
+    if (this.engine) {
+      this.stop();
+    }
+
+    this.engine = new ProviderEngine({pollingInterval: 20000});
+
+    this.engine.addProvider(new CacheSubprovider());
+    this.engine.addProvider(new FilterSubprovider());
+    this.engine.addProvider(new HookedWalletEthTxSubprovider({
+      getAccounts: (cb) => {
+        cb(null, this.selectedAddress ? [this.selectedAddress] : []);
+      },
+      getPrivateKey: (address, cb) => {
+        cb(null, this.walletStorage.privateKeyOf(this.selectedAddress));
+      }
+    }));
+    this.engine.addProvider(new RpcSubprovider({rpcUrl: this.rpcUrl}));
+
+    if (blockTracker) {
+      this.engine.on('block', blockTracker);
+    }
+
+    this.engine.start();
+  }
+
+  sendAsync(...args) {
+    this.engine.sendAsync.apply(this.engine, args);
+  }
+
+  stop() {
+    this.engine.stop();
+  }
+
+  changeAddress(address: string) {
+    this.selectedAddress = address;
+  }
+}
