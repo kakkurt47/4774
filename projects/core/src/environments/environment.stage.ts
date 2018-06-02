8f1409aa0d4b05c9c216f82fcc756865ceec0dbd
Heo Sangmin <heo@mapiacompany.com>
2018-06-02 14:25:34 +0900

    ci: contract:compile command include import process

--- projects/core/src/environments/environment.stage.ts
+++ projects/core/src/environments/environment.stage.ts
@@ -3,7 +3,7 @@ import {EnvironmentType} from './env_types';
 export const environmentStage: EnvironmentType = {
   production: false,
   env: 'stage',
-  infuraAccessToken: 'nCLC9iatDyYYhUaFCPkZ',
+  infuraAccessToken: 'BHFyoHFDkG2NGioaIlf4',
   rpcUrl: 'https://ropsten.infura.io',
   networkId: 3,
 };
--- projects/viewer/node_src/environment.ts
+++ projects/viewer/node_src/environment.ts
@@ -1,4 +1,4 @@
 export const electronEnvironment = {
   production: false,
-  env: 'dev'
+  env: 'stage'
 };
