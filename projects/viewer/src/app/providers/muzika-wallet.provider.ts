92a4db82ab2ce4badb87fc2f123239c9ceb86d34
Jeong Chaehong <brcps12@gmail.com>
2018-06-05 11:15:09 +0900

    lint fix

--- projects/viewer/src/app/providers/muzika-wallet.provider.ts
+++ projects/viewer/src/app/providers/muzika-wallet.provider.ts
@@ -100,7 +100,7 @@ export class MuzikaWalletProvider implements Web3Provider {
   }
 
   private uuid(): string {
-    const s4 = () => ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
   }
 
