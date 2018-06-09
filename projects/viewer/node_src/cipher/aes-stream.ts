cef4cc7bf929a10327d0625c58fbe60ba06fe3ee
seungwon-kang <ksw3894@gmail.com>
2018-06-09 17:58:42 +0900

    fix(aes-stream): insert IV if pushIv option is set and push null if stream ended

--- projects/viewer/node_src/cipher/aes-stream.ts
+++ projects/viewer/node_src/cipher/aes-stream.ts
@@ -1,9 +1,12 @@
 /**
- * This class supports stream to encrypt or to decrypt by AES.
+ * This class supports stream to encrypt or to decrypt by AES. Because of CBC encryption, the read chunk length should be the multiple
+ * of initializer vector length (default 16)
  */
 
+import * as crypto from 'crypto';
 import * as aesjs from 'aes-js';
 import {Transform} from 'stream';
+import {BlockUtil} from '../block/block';
 
 
 export class AESCBCEncryptionStream extends Transform {
@@ -11,17 +14,22 @@ export class AESCBCEncryptionStream extends Transform {
   blockSize: number;
   _aes: any;
 
-  constructor(key, iv, options) {
+  constructor(options) {
     super(options);
-    this.prevVector = iv;
-    this.blockSize = iv.length;
-    this._aes = new aesjs.AES(key);
+    this.prevVector = options.iv || crypto.randomBytes(BlockUtil.IV_SIZE);
+    this.blockSize = this.prevVector.length;
+    this._aes = new aesjs.AES(options.key);
+
+    // if pushIv option is true or undefined, push IV to the front
+    if (typeof options.pushIv === 'undefined' || options.pushIv) {
+      this.push(this.prevVector);
+    }
   }
 
   _transform(data, encoding, callback) {
     // if the data length is not multiplied by vector size
     if (data.length % this.blockSize) {
-      callback(new Error(`invalid data length (data length : ${data.length})`), null);
+      callback(new Error(`invalid data length (data length : ${data.length})`));
     }
 
     for (let i = 0; i < data.length; i += this.blockSize) {
@@ -35,7 +43,13 @@ export class AESCBCEncryptionStream extends Transform {
       this.prevVector.copy(data, i);
     }
 
-    callback(null, data);
+    this.push(data);
+    callback();
+  }
+
+  _flush(callback) {
+    this.push(null);
+    callback();
   }
 }
 
@@ -45,14 +59,30 @@ export class AESCBCDecryptionStream extends Transform {
   blockSize: number;
   _aes: any;
 
-  constructor(key, iv, options) {
+  constructor(options) {
     super(options);
-    this.prevVector = iv;
-    this.blockSize = iv.length;
-    this._aes = new aesjs.AES(key);
+    this.prevVector = options.iv;
+
+    if (this.prevVector) {
+      this.blockSize = this.prevVector.length;
+    }
+
+    this._aes = new aesjs.AES(options.key);
   }
 
   _transform(data, encoding, callback) {
+    // if IV is not initialized, read IV from first buffer array.
+    if (!this.prevVector) {
+      this.prevVector = data.slice(0, BlockUtil.IV_SIZE);
+      this.blockSize = this.prevVector.length;
+      if (data.length === BlockUtil.IV_SIZE) {
+        callback();
+        return;
+      } else {
+        data = data.slice(BlockUtil.IV_SIZE);
+      }
+    }
+
     // if the data length is not multiplied by vector size
     if (data.length % this.blockSize) {
       callback(new Error(`invalid data length (data length : ${data.length})`), null);
@@ -72,7 +102,13 @@ export class AESCBCDecryptionStream extends Transform {
       this.prevVector = cipherBlock;
     }
 
-    callback(null, data);
+    this.push(data);
+    callback();
+  }
+
+  _flush(callback) {
+    this.push(null);
+    callback();
   }
 }
 
