22f43408cf1ef9c7824487b762b94bf41e4dd296
Heo Sangmin <heo@mapiacompany.com>
2018-07-25 17:07:04 +0900

    hotfix: link error fix

--- projects/web/src/modules/intro/components/intro-mzk-coins.component.ts
+++ projects/web/src/modules/intro/components/intro-mzk-coins.component.ts
@@ -13,8 +13,8 @@ import { BaseComponent } from '@muzika/core/angular';
           {{'intro-mzk-coins.subtitle-1' | translate}}<BR/> <BR/>
           {{'intro-mzk-coins.subtitle-2' | translate}}
         </p>
-        <img src="assets/intro-img/diagram-coin_pc.png" class="mt-sm-5 img-fluid mb-sm-5 hidden-pc">
-        <img src="assets/intro-img/diagram-coin_mobile.png" class="mt-sm-5 img-fluid mb-sm-5 hidden-mb">
+        <img src="assets/intro-img/diagram_coin_pc.png" class="mt-sm-5 img-fluid mb-sm-5 hidden-pc">
+        <img src="assets/intro-img/diagram_coin_mobile.png" class="mt-sm-5 img-fluid mb-sm-5 hidden-mb">
         <BR/>
         <BR/>
         <h2 class="mt-5 text-center intro-section-title mb-5">{{'intro-mzk-coins.major-functions' | translate}}</h2>
