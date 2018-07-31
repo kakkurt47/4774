fc461281536c7963b569278f2a40ff8838e99959
Heo Sangmin <heo@mapiacompany.com>
2018-07-31 10:12:06 +0900

    refactor(content-exchange): ui setup

--- projects/intro/src/modules/airdrop/component/content-exchange/content-exchange.component.ts
+++ projects/intro/src/modules/airdrop/component/content-exchange/content-exchange.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { interval } from 'rxjs';
 
+declare const jQuery;
+
 @Component({
   selector: 'airdrop-content-exchange',
   template: `
@@ -27,15 +29,26 @@ import { interval } from 'rxjs';
         더 많은 LP를 획득할수록 여러분은 커뮤니티에 더 많은 영향력을 갖게 됩니다.
       </li>
     </ul>
-    
-    <div class="receive-box mt-4">
+
+    <div class="receive-box one mt-4">
       <h5 class="text-center">
-        수령코드 
-        <i class="receive-code">{{randomCode}}</i>
+        MyEtherWallet 이더리움 지갑 주소를 입력해주세요.
       </h5>
-      <input type="text" class="form-control mt-4" placeholder="이더리움 지갑 주소 입력하기">
-      <ul class="mt-4">
-        <li> MyEtherWallet 지갑 주소를 입력해주세요. </li>
+      <div class="input-group mt-4">
+        <input type="text" class="form-control" placeholder="지갑 주소 입력하기">
+        <button class="btn btn-default btn-sm" (click)="secondShow()">
+          확인
+        </button>
+      </div>
+      <div class="text-danger text-center mt-4"> 주소가 없거나, 거래소 지갑 주소인 경우 수령하실 수 없습니다. </div>
+    </div>
+
+    <div class="receive-box two mt-5">
+      <h5 class="text-center">
+        본인 확인용 수령코드 
+        <i class="receive-code">{{randomCode}}</i>  
+      </h5>
+      <ul class="mt-4 instruction">
         <li> 텔레그램 (https://t.me/muzika_korean)에 입장 후 수령코드를 남겨주셔야 중복 확인 후 지급됩니다. </li>
         <li> MZK코인은 상장 이전에 입력하신 지갑 주소로 분배 될 예정이며, 구체적인 일정은 텔래그램을 통해 공지해드리겠습니다. </li>
         <li> 텔래그램 그룹에서 MZK 상장 등 여러 소식을 확인하세요. 감사합니다. </li>
@@ -60,15 +73,36 @@ export class AirdropContentExchangeComponent {
     if (this.isStart) {
       return;
     }
-    const previousLP = this.lp;
-    const sub = interval(50)
+    const sub = interval(30)
       .subscribe(i => {
         this.isStart = true;
         this.lp -= 1;
         this.mzk = Math.floor((i + 1) / 5);
-        if (this.lp === 0) {
+        if (this.lp <= 0) {
           sub.unsubscribe();
+          jQuery('.receive-box.one').show('slow');
+
         }
       });
+  }
+
+  secondShow() {
+    jQuery('.receive-box.two').show('slow');
+    jQuery('.instruction li').each((index, tag) => {
+      const timing = index * 1300;
+      setTimeout(() => {
+        jQuery(tag).show('slow');
+      }, timing);
+    });
+  }
+
+  copy () {
+    const copyText = jQuery('.receive-code');
+
+    /* Select the text field */
+    copyText.select();
+
+    /* Copy the text inside the text field */
+    document.execCommand('copy');
   }
 }
