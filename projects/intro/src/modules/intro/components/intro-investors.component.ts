767719f95c8b90c8403e6f5b5188ee55b3004eee
Heo Sangmin <heo@mapiacompany.com>
2018-09-17 14:51:15 +0900

    feat(investors): add vestigium

--- projects/intro/src/modules/intro/components/intro-investors.component.ts
+++ projects/intro/src/modules/intro/components/intro-investors.component.ts
@@ -27,12 +27,17 @@ import { BaseComponent } from '../../../models/base.component';
           </div>
           <div class="col-sm-3 col-6">
             <div class="wrap-box">
-              <img src="assets/intro-img/partners/logo-blackhorse.png" class="img-fluid">
+              <img src="assets/intro-img/investors/vestigium.png" class="img-fluid">
             </div>
           </div>
         </div>
         <div class="clearfix"></div>
         <div class="row mt-sm-4">
+          <div class="col-sm-3 col-6">
+            <div class="wrap-box">
+              <img src="assets/intro-img/partners/logo-blackhorse.png" class="img-fluid">
+            </div>
+          </div>
           <div class="col-sm-3 col-6">
             <div class="wrap-box">
               <img src="assets/intro-img/partners/logo-everest-ventures.png" class="img-fluid">
@@ -47,9 +52,6 @@ import { BaseComponent } from '../../../models/base.component';
             <div class="wrap-box">
               <img src="assets/intro-img/partners/logo-ulysses-capital.png" class="img-fluid">
             </div>
-          </div>
-          <div class="col-sm-3 col-6">
-            <mzk-intro-countdown [expiredDate]="expiredDates[0]"></mzk-intro-countdown>
           </div>
         </div>
       </div>
